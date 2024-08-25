5b68a01109e21de2ddc4fcccc0bfedf8b79dc198
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-08-25 14:10:39 -0500

    Adición de README

---
+++ README.md
@@ -0,0 +1,7 @@
+# CODELAB 3 - EJERCICIO 4: CUADRANTE COMPOSE
+
+## Vista previa
+![imagen_1](Vista_previa.png)
+
+## Ejecución en dispositivo físico
+![imagen_2](Celular.png)
--- app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
+++ app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
@@ -3,18 +3,15 @@ package com.example.cuadrantecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+//import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +28,7 @@ import com.example.cuadrantecompose.ui.theme.CuadranteComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             CuadranteComposeTheme {
                 Surface(
